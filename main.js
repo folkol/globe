3b96b58da2cdfe45b4a382cb3e1a71535a8fefda
Matte <mattias4@kth.se>
2018-07-25 21:57:46 +0200

    Just rendering one face now.

--- main.js
+++ main.js
@@ -91,31 +91,31 @@ function initBuffers(gl) {
         // Front face
         -1.0, -1.0, 1.0,
         1.0, -1.0, 1.0,
-        1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, -1.0, 1.0, 1.0
 
-        // Back face
-        -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
-        1.0, 1.0, -1.0,
-        1.0, -1.0, -1.0,
+        // // Back face
+        // -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
+        // 1.0, 1.0, -1.0,
+        // 1.0, -1.0, -1.0,
 
-        // Top face
-        -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0,
-        1.0, 1.0, -1.0,
+        // // Top face
+        // -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,
+        // 1.0, 1.0, 1.0,
+        // 1.0, 1.0, -1.0,
 
-        // Bottom face
-        -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-        1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
+        // // Bottom face
+        // -1.0, -1.0, -1.0,
+        // 1.0, -1.0, -1.0,
+        // 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
 
-        // Right face
-        1.0, -1.0, -1.0,
-        1.0, 1.0, -1.0,
-        1.0, 1.0, 1.0,
-        1.0, -1.0, 1.0,
+        // // Right face
+        // 1.0, -1.0, -1.0,
+        // 1.0, 1.0, -1.0,
+        // 1.0, 1.0, 1.0,
+        // 1.0, -1.0, 1.0,
 
-        // Left face
-        -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
+        // // Left face
+        // -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -165,43 +165,13 @@ function initBuffers(gl) {
         1.0, 0.0,
         1.0, 1.0,
         0.0, 1.0,
-        // Back
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Top
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Bottom
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Right
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Left
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
     ];
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     const indices = [
-        0, 1, 2, 0, 2, 3, // front
-        4, 5, 6, 4, 6, 7, // back
-        8, 9, 10, 8, 10, 11, // top
-        12, 13, 14, 12, 14, 15, // bottom
-        16, 17, 18, 16, 18, 19, // right
-        20, 21, 22, 20, 22, 23, // left
+        0, 1, 2, 0, 2, 3
     ];
 
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
@@ -373,7 +343,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     {
-        const vertexCount = 36;
+        const vertexCount = 6;
         const type = gl.UNSIGNED_SHORT;
         const offset = 0;
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
