77c3d5b1a496800b6701d4f8dd84d3069bd59058
Matte <mattias4@kth.se>
2018-07-25 21:56:05 +0200

    Scaled the canvas up a bit, and removed more noise

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 var cubeRotation = 0.0;
+var speed = 0.1;
 
 main();
 
-//
-// Start here
-//
 function main() {
     const canvas = document.querySelector('#glcanvas');
     const gl = canvas.getContext('webgl');
-
-    // If we don't have a GL context, give up now
 
     if (!gl) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.');
         return;
     }
-
-    // Vertex shader program
 
     const vsSource = `
     attribute vec4 aVertexPosition;
@@ -47,8 +41,6 @@ function main() {
     }
   `;
 
-    // Fragment shader program
-
     const fsSource = `
     varying highp vec2 vTextureCoord;
     varying highp vec3 vLighting;
@@ -62,14 +54,7 @@ function main() {
     }
   `;
 
-    // Initialize a shader program; this is where all the lighting
-    // for the vertices and so forth is established.
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-
-    // Collect all the info needed to use the shader program.
-    // Look up which attributes our shader program is using
-    // for aVertexPosition, aVertexNormal, aTextureCoord,
-    // and look up uniform locations.
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
@@ -85,22 +70,15 @@ function main() {
         },
     };
 
-    // Here's where we call the routine that builds all the
-    // objects we'll be drawing.
     const buffers = initBuffers(gl);
-
-    const texture = loadTexture(gl, 'cubetexture.png');
+    const texture = loadTexture(gl, 'earthspec1k.jpg');
 
     var then = 0;
-
-    // Draw the scene repeatedly
     function render(now) {
         now *= 0.001; // convert to seconds
         const deltaTime = now - then;
         then = now;
-
         drawScene(gl, programInfo, buffers, texture, deltaTime);
-
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
@@ -235,19 +213,10 @@ function initBuffers(gl) {
     };
 }
 
-//
-// Initialize a texture and load an image.
-// When the image finished loading copy it into the texture.
-//
 function loadTexture(gl, url) {
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    // Because images have to be download over the internet
-    // they might take a moment until they are ready.
-    // Until then put a single pixel in the texture so we can
-    // use it immediately. When the image has finished downloading
-    // we'll update the texture with the contents of the image.
     const level = 0;
     const internalFormat = gl.RGBA;
     const width = 1;
@@ -266,15 +235,9 @@ function loadTexture(gl, url) {
         gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
             srcFormat, srcType, image);
 
-        // WebGL1 has different requirements for power of 2 images
-        // vs non power of 2 images so check if the image is a
-        // power of 2 in both dimensions.
         if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-            // Yes, it's a power of 2. Generate mips.
             gl.generateMipmap(gl.TEXTURE_2D);
         } else {
-            // No, it's not a power of 2. Turn of mips and set
-            // wrapping to clamp to edge
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
@@ -289,25 +252,13 @@ function isPowerOf2(value) {
     return (value & (value - 1)) == 0;
 }
 
-//
-// Draw the scene.
-//
 function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
-    // Clear the canvas before we start drawing on it.
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
 
     const fieldOfView = 45 * Math.PI / 180; // in radians
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
@@ -329,7 +280,6 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 
     // Now move the drawing position a bit to where we want to
     // start drawing the square.
-
     mat4.translate(modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to translate
         [-0.0, 0.0, -6.0]); // amount to translate
@@ -346,8 +296,6 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     mat4.invert(normalMatrix, modelViewMatrix);
     mat4.transpose(normalMatrix, normalMatrix);
 
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute
     {
         const numComponents = 3;
         const type = gl.FLOAT;
@@ -406,15 +354,8 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
             programInfo.attribLocations.vertexNormal);
     }
 
-    // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-
-    // Tell WebGL to use our program when drawing
-
     gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.projectionMatrix,
         false,
@@ -427,16 +368,8 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
         programInfo.uniformLocations.normalMatrix,
         false,
         normalMatrix);
-
-    // Specify the texture to map onto the faces.
-
-    // Tell WebGL we want to affect texture unit 0
     gl.activeTexture(gl.TEXTURE0);
-
-    // Bind the texture to texture unit 0
     gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    // Tell the shader we bound the texture to texture unit 0
     gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     {
@@ -446,27 +379,17 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
     }
 
-    // Update the rotation for the next draw
-
-    cubeRotation += deltaTime;
+    cubeRotation -= deltaTime * speed;
 }
 
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-//
 function initShaderProgram(gl, vsSource, fsSource) {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    // Create the shader program
 
     const shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
-
-    // If creating the shader program failed, alert
-
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
         alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
         return null;
@@ -475,23 +398,11 @@ function initShaderProgram(gl, vsSource, fsSource) {
     return shaderProgram;
 }
 
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
 function loadShader(gl, type, source) {
     const shader = gl.createShader(type);
 
-    // Send the source to the shader object
-
     gl.shaderSource(shader, source);
-
-    // Compile the shader program
-
     gl.compileShader(shader);
-
-    // See if it compiled successfully
-
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
